bbce0f7a37786c6e73918f880e474e810e905ec0
fastfabien <fastfabien56@gmail.com>
2024-04-23 13:09:55 +0300

    First config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#2E933C",
+        secondary: "#204E4A",
+        teritiary: "#297045",
+        quantenary: "81C14B",
+        rose: "#ECBA82",
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+        "7xl": "5rem",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "button-gradient": "linear-gradient(140deg, #204E4A, #2E933C)",
+        "background-gradient": "linear-gradient(140deg, #204E4A, #2E933C)",
       },
     },
   },
